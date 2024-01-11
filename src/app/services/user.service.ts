02e59b05a7723c739cd6bd97e01abc74ac45b363
Amappola <anam.porrasp@gmail.com>
2024-01-11 11:13:46 -0500

    Creating an async validator

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, every, first, map, of } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private _url: string = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this._url).pipe(first());
+  }
+
+  isEmailUnique(email: string): Observable<boolean | null> {
+    return this.getUsers()
+      .pipe(
+        map((users) => users.map((user) => user.email !== email)),
+        every(results => { console.log(results); return false }
+        ));
+  }
 }
