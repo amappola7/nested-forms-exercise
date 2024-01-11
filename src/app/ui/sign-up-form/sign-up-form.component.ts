32c1fef0fc1db7d22126ebe4eccf9c6b49b0c7db
Amappola <anam.porrasp@gmail.com>
2024-01-11 08:26:15 -0500

    Setting up the sign up form

--- src/app/ui/sign-up-form/sign-up-form.component.ts
+++ src/app/ui/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up-form.component.scss']
 })
 export class SignUpFormComponent {
+  signUpForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required, Validators.min(4)]],
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.max(80),
+          Validators.pattern('[a-zA-Z ]*'),
+        ],
+      ],
+      secondName: ['', [Validators.max(80), Validators.pattern('[a-zA-Z ]*')]],
+      surname: [
+        '',
+        [
+          Validators.required,
+          Validators.max(80),
+          Validators.pattern('[a-zA-Z ]*'),
+        ],
+      ],
+      secondSurname: [
+        '',
+        [Validators.max(80), Validators.pattern('[a-zA-Z ]*')],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.min(6),
+          Validators.pattern('^(?=.*[a-zA-Z])(?=.*d)(?=.*[#$%&_-]).*$'),
+        ],
+      ],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', [Validators.required]],
+        zip: [-1, [Validators.min(5)]]
+      })
+    });
+  };
+
+  onSubmit(){};
 }
