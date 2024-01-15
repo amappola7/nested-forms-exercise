3b3b1274780cc2306cde3f9f86375412388fd930
Amappola <anam.porrasp@gmail.com>
2024-01-15 09:07:58 -0500

    Documenting the user.service methods

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,14 +11,23 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Get users list from the API
+   * @returns An observable with the list of users
+   */
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this._url).pipe(first());
   }
 
+  /**
+   * Create a new user in the API
+   * @param userData An object of type User with all the user information to be stored in the API
+   * @returns If the request is correct, it won't return nothing but a message in the console with the result. If there is an error, it will return null and an error message in the console
+   */
   createUser(userData: User): Observable<User | null> {
     return this.http.post<User>(this._url, userData)
     .pipe(
-      tap(() => console.log('User successfully created', userData)),
+      tap(() => console.log('User successfully created')),
       catchError((error) => {
         console.log('Error creating user', error);
         return of(null);
@@ -26,13 +35,18 @@ export class UserService {
     )
   }
 
-  isEmailUnique(email: string): Observable<boolean> {
+  /**
+   * Check if the entered email is unique or not compared to all users emails
+   * @param email The email entered by the user
+   * @returns true if the email is unique, false otherwise. If there is an error, it will return null and an error message in the console
+   */
+  isEmailUnique(email: string): Observable<boolean | null> {
     return this.getUsers()
       .pipe(
         map((users) => users.every((user) => user.email !== email)),
         catchError((error) => {
           console.log('Error while trying to validate if email is unique:', error);
-          return of(false);
+          return of(null);
         })
       );
   }
