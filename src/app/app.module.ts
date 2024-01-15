bda7188de0a293f433dc6bfbc424854ef13fb48c
Amappola <anam.porrasp@gmail.com>
2024-01-15 08:53:47 -0500

    Moving the sign-up-form component to a new folder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignUpFormComponent } from './ui/sign-up-form/sign-up-form.component';
+import { SignUpFormComponent } from './ui/components/sign-up-form/sign-up-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 
