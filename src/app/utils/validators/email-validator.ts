ec935144d79d3d57bd9fe02044080f11afd835f8
Amappola <anam.porrasp@gmail.com>
2024-01-15 09:18:20 -0500

    Commenting validator and fixing some bugs

--- src/app/utils/validators/email-validator.ts
+++ src/app/utils/validators/email-validator.ts
@@ -9,6 +9,11 @@ import { UserService } from "src/app/services/user.service";
 export class UniqueEmailValidator implements AsyncValidator {
   constructor(private userService: UserService) { }
 
+  /**
+   * An Angular custom validator for an email control in a form. It checks if the email already exists in the API or not.
+   * @param control The input type email control
+   * @returns null if the email doesn't exist, otherwise, a validation error
+   */
   validate(control: AbstractControl): Observable<any> {
     return this.userService.isEmailUnique(control.value).pipe(
       map((isUnique) => (isUnique ? null : {uniqueEmail: true})),
